fcb4f965d292365449d74b265075dc9750a3aea6
Michael Jones <michael@pisignage.co.uk>
2016-11-11 08:38:51 +0000

    Edit

--- vol.php
+++ vol.php
@@ -5,13 +5,14 @@ $radio = '/usr/local/bin/piradio';
 if (isset($_GET['adj'])) {
 	// Volume adjustment may consist of digits or be + or -
 	$adj = preg_replace('/[^0-9+-]+/', '', $_GET['adj']);
-	if (strlen($adj))
+	if (strlen($adj)) {
 		if (!strcmp($adj, '-') || !strcmp($adj, '+') || is_numeric($adj)) {
 			// Adjust volume, discard output
 			exec($radio . ' vol ' . $adj);
 		} elseif(!strcmp($adj, 'mute')) {
 			exec($radio . $adj);
 		}
+	}
 }
 
 // Get status, display output
