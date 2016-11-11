ffa608f675748cfbadabfb1fa67dec82e5432a02
Michael Jones <michael@pisignage.co.uk>
2016-11-11 08:36:38 +0000

    Add Mute

--- vol.php
+++ vol.php
@@ -6,9 +6,12 @@ if (isset($_GET['adj'])) {
 	// Volume adjustment may consist of digits or be + or -
 	$adj = preg_replace('/[^0-9+-]+/', '', $_GET['adj']);
 	if (strlen($adj))
-		if (!strcmp($adj, '-') || !strcmp($adj, '+') || !strcmp($adj, 'mute') ||s_numeric($adj))
+		if (!strcmp($adj, '-') || !strcmp($adj, '+') || is_numeric($adj)) {
 			// Adjust volume, discard output
 			exec($radio . ' vol ' . $adj);
+		} elseif(!strcmp($adj, 'mute')) {
+			exec($radio . $adj);
+		}
 }
 
 // Get status, display output
